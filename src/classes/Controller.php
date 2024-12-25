a1f0adb91e6314b043e02df506e73ab22cc355f7
Taha <kyojin.rar@gmail.com>
2024-12-25 15:13:11 +0100

    FEAT: enhance Controller class with error and basic response methods, and add request data retrieval

--- src/classes/Controller.php
+++ src/classes/Controller.php
@@ -2,7 +2,7 @@
 
 class Controller {
     
-    protected function successResponse ($data, $msg = 'Success',$code) {
+    protected function successResponse ($data, $msg = 'Success',$code = '200') {
         http_response_code($code);
         header('Content-Type: application/json');
         echo json_encode([
@@ -13,5 +13,25 @@ class Controller {
         exit;
     }
 
-    
+    protected function errResponse ($msg, $code = '400') {
+        http_response_code($code);
+        header('Content-Type: application/json');
+        echo json_encode([
+            'status' => 'error',
+            'message' => $msg
+        ]);
+        exit;
+    }
+
+    protected function basicResponse ($data, $code = '200') {
+        http_response_code($code);
+        header('Content-Type: application/json');
+        echo json_encode($data);
+        exit;
+    }
+
+    protected function getRequestData () {
+        $data = json_decode(file_get_contents('php://input'));
+        return $data;
+    } 
 }

af488f12f8f98dfb712acda2df0bb866b2d90f73
Taha <kyojin.rar@gmail.com>
2024-12-25 12:40:48 +0100

    FEAT: add debug and security helper functions for later use

--- src/loader.php
+++ src/loader.php
@@ -1,7 +1,14 @@
 <?php
 
+
+//DB CONFIG
 require_once __DIR__ . '/config/config.php';
 
+//helpers
+require_once __DIR__ . './helpers/secure.php';
+require_once __DIR__ . './helpers/debug.php';
+
+//AUTO LOADER
 spl_autoload_register(function($className){
     require_once __DIR__ . '/classes/' . $className . '.php';
 });
